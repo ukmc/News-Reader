4aa5f767a6c5debdb3a3a881a951b9e1a7068c7d
Utku Kamacı <ukmc.j0@gmail.com>
2017-07-10 19:50:37 +0300

    working with an uıimage bug

--- News Reader/News Reader/ViewController.swift
+++ News Reader/News Reader/ViewController.swift
@@ -9,18 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
     @IBOutlet weak var tableview: UITableView!
 
+    var articles: [Article]? = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchArticles()
+        
+    }
+    
+    func fetchArticles() {
+        let urlRequest = URLRequest(url: URL(string: "https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=57be79757786465fb152b7846b33a214")!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data,response,error) in
+            
+            if error != nil {
+                print(error)
+                return
+                
+            }
+            
+            self.articles = [Article]()
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                
+                if let articlesFromJson = json["articles"] as? [[String : AnyObject]] {
+                    for articleFromJson in articlesFromJson {
+                        let article = Article()
+                        if let title = articleFromJson["title"] as? String, let author = articleFromJson["author"] as? String, let desc = articleFromJson["description"] as? String, let url = articleFromJson["url"] as? String, let urlToImage = articleFromJson["urlToImage"] as? String {
+                            
+                            article.author = author
+                            article.desc = desc
+                            article.headline = title
+                            article.url = url
+                            article.imageUrl = urlToImage
+                        }
+                        self.articles?.append(article)
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.tableview.reloadData()
+                }
+                
+            } catch let error {
+                print(error)
+            }
+            
+        }
+        
+        task.resume()
+        
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as! ArticleCell
         
-       cell.title.text = "THIS IS A TESTING TEXT FOR TITTTTTTTLEEEE"
+        cell.title.text = self.articles?[indexPath.item].headline
+        cell.desc.text = self.articles?[indexPath.item].desc
+        cell.author.text = self.articles?[indexPath.item].author
+        cell.imgView.downloadImage(from: (self.articles?[indexPath.item].imageUrl)!)
         
         return cell
         
@@ -32,9 +81,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.articles?.count ?? 0
         
     }
 
 }
+
+extension UIImageView {
+    
+    func downloadImage(from url: String) {
+        
+        let urlRequest = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data,response,error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data!)
+            }
+        }
+        task.resume()
+    }
+}
+
+
+
+
+
+
+
+
 
